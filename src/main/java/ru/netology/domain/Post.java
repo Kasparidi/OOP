803a15c0f32ff8e4e40b8e2646f2ad2cf4a232eb
Kasparidi <kasparidi@gmail.com>
2020-10-14 11:08:19 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Post {
+    private int postId;
+    private int ownerWallId;
+    private int authorId;
+    private int data;
+    private String text;
+    private String pictureUrl;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private GeoInfo geoInfo;
+    private int canPin;
+    private int canDelete;
+    private boolean bookmarks;
+
+
+
+
+}
