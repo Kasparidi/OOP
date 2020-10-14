b912ff4d741ed5968414f382adab55d1455dc1f4
Kasparidi <kasparidi@gmail.com>
2020-10-14 17:58:56 +0300

    added package manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    public void delete(int postId, int ownerWallId) {
+    }
+
+    public Post[] search(int ownerWallId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return search();
+    }
+}
